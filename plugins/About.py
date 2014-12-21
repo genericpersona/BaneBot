716749372fd5cd1c82f60f90e72f111d50e7b9b8
genericpersona <generic.persona@yahoo.com>
2014-12-21 03:11:12 -0500

    Added github link.

--- plugins/About.py
+++ plugins/About.py
@@ -3,7 +3,8 @@
 import plugins.PluginBase as pb
 
 class About(pb.CommandPlugin):
-  ABOUT = 'Based on BaneBot by genericpersona.'
+  ABOUT = 'Based on BaneBot by genericpersona.' + \
+          'https://github.com/genericpersona/BaneBot'
   def __init__(self, conf):
     super(About, self).__init__(conf)
 
